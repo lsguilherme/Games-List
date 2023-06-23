dcb81ac9108273eac4fc042b3347c0d23c583f3c
lsguilherme <lacerdagui42@gmail.com>
2023-06-23 20:55:34 -0300

    Ajustes no Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,25 @@
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import "./styles.css";
 
 export function Footer() {
   return (
     <footer className="footer-container">
-      <h1>Teste</h1>
+      <div className="social-container">
+        <a
+          href="https://github.com/lsguilherme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/guilherme-lacerda-498996210/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
     </footer>
   );
 }

af7b0f6f07dc61a83281d681550a86b6e0e31532
lsguilherme <lacerdagui42@gmail.com>
2023-06-24 18:27:52 -0300

    Ajuste em estilos

--- src/components/EmptyGame/index.jsx
+++ src/components/EmptyGame/index.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export function EmptyGame() {
   return (
     <div>
-      <p className="empty-game">Nenhum jogo encontrado!</p>
+      <p className="empty-game">Nenhum jogo encontrado, tente outro nome.</p>
     </div>
   );
 }

c1add9c08f3efd1c171ad64c42f67c2169f4dcc1
lsguilherme <lacerdagui42@gmail.com>
2023-07-16 00:07:10 -0300

    estilos

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,27 @@
+import { useContext } from "react";
 import "./styles.css";
 import { FaGamepad } from "react-icons/fa";
+import { AuthContext } from "../../context/AuthContext";
 
 export function Header() {
+  const { uid } = useContext(AuthContext);
   const handleIconClick = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
     <header className="header-container">
+      <div className="empty" />
       <div onClick={handleIconClick}>
         <FaGamepad />
         <p>Game List</p>
       </div>
+      {!uid ? (
+        <a className="container-button-login" href="/auth">
+          Login
+        </a>
+      ) : (
+        <div className="empty" />
+      )}
     </header>
   );
 }
